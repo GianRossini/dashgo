68c8c85ec952477d79968482f47a12b52f7479c2
GianRossini <gianrossini.developer@gmail.com>
2022-01-18 20:34:56 -0300

    creates sign in and header components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,42 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
+import { Flex, Button, Stack } from '@chakra-ui/react';
+
+import { Input } from '../components/Form/Input';
+
+const SignIn: NextPage = () => {
   return (
-    <h1>Hello World!</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="Email" type="email" />
+          <Input name="password" label="Password" type="password" />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Sign In
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
 
-export default Home
+export default SignIn
