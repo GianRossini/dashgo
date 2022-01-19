c25802605fb7e5dcdf688cbccb72f90b8751e71b
GianRossini <gianrossini.developer@gmail.com>
2022-01-19 19:43:20 -0300

    creates sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,22 @@
+import { Flex } from '@chakra-ui/react';
+
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex h="100vh" direction="column">
+      <Header />
+
+      <Flex
+        w="100%"
+        maxW={1480}
+        mx="auto"
+        my="6"
+        px="6"
+      >
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
