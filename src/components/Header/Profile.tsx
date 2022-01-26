c6e2670702912309a5278400cb6bca12d5558e56
GianRossini <gianrossini.developer@gmail.com>
2022-01-26 14:06:06 -0300

    turns header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean | undefined;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return(
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>User Name</Text>
-        <Text color="gray.300" fontSize="small">
-          user@email.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>User Name</Text>
+          <Text color="gray.300" fontSize="small">
+            user@email.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="User Name" />
     </Flex>
