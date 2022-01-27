f948a14d1c026439260c88de48ff5c1833ee4beb
GianRossini <gianrossini.developer@gmail.com>
2022-01-27 15:24:58 -0300

    creates active link condition

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -2,24 +2,24 @@ import Link, { LinkProps as NextLinkProps } from 'next/link';
 
 import { Link as ChakraLink, LinkProps as ChakraLinkProps, Icon, Text } from '@chakra-ui/react';
 
+import { ActiveLink } from '../ActiveLink';
+
 import { IconType } from 'react-icons';
 
 interface NavLinkProps {
-  icon: IconType,
-  children: React.ReactNode,
-  nextLinkProps: NextLinkProps,
-  chakraLinkProps?: ChakraLinkProps,
+  icon: IconType;
+  children: React.ReactNode;
+  href: string;
+  chakraLinkProps?: ChakraLinkProps;
 }
 
-export function NavLink({ icon, children, nextLinkProps, chakraLinkProps }: NavLinkProps) {
+export function NavLink({ icon, children, href, chakraLinkProps }: NavLinkProps) {
   return(
-    <Link {...nextLinkProps}>
-      <a>
-        <ChakraLink display="flex" align="center" {...chakraLinkProps}>
-          <Icon as={icon} fontSize="20" />
-          <Text ml="4" fontWeight="medium">{children}</Text>
-        </ChakraLink>
-      </a>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display="flex" align="center" {...chakraLinkProps}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </ChakraLink>
+    </ActiveLink>
   );
 }
--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -9,13 +9,13 @@ export function SidebarNav() {
   return(
     <Stack spacing="12" align="flex-start">
       <NavSection title="GENERAL">
-        <NavLink nextLinkProps={{ href: '/dashboard' }} icon={RiDashboardLine}>Dashboard</NavLink>
-        <NavLink nextLinkProps={{ href: '/users' }} icon={RiContactsLine}>Users</NavLink>
+        <NavLink href="/dashboard" icon={RiDashboardLine}>Dashboard</NavLink>
+        <NavLink href="/users" icon={RiContactsLine}>Users</NavLink>
       </NavSection>
 
       <NavSection title="AUTOMATION">
-        <NavLink nextLinkProps={{ href: '/forms' }} icon={RiInputMethodLine}>Forms</NavLink>
-        <NavLink nextLinkProps={{ href: '/automation' }} icon={RiGitMergeLine}>Automation</NavLink>
+        <NavLink href="/forms" icon={RiInputMethodLine}>Forms</NavLink>
+        <NavLink href="/automation" icon={RiGitMergeLine}>Automation</NavLink>
       </NavSection>
     </Stack>
   );
