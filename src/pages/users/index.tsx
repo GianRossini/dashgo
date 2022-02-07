ee4db6d7d67eb291ace3ec4664a3f01d4184c55e
GianRossini <gianrossini.developer@gmail.com>
2022-02-07 20:15:20 -0300

    configures axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 
 import { useQuery } from 'react-query';
 
+import { api } from '../../services/api';
+
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
@@ -29,8 +31,7 @@ import {
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json();
+    const { data } = await api.get('users');
 
     const users = data.users.map(user => ({
       id: user.id,
